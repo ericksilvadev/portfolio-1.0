b193114b9ae92044e3415cc0d5ace5d44495bc74
Erick Silva <erick.silva6653@gmail.com>
2021-05-18 00:08:11 -0300

    remove coisas desnecessaria no main js

--- main.js
+++ main.js
@@ -25,13 +25,11 @@ let active = false;
 
 burgerBtn.addEventListener('click', () => {
   if (!active) {
-    home.classList.add('active');
     nav.classList.add('display');
     navMenu.classList.add('display');
     burgerBtn.classList.add('active');
     active = true;
   } else {
-    home.classList.remove('active');
     burgerBtn.classList.remove('active');
     navMenu.classList.remove('display');
     nav.classList.remove('display');
